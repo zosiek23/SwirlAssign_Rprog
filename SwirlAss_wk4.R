7106b39fe25b48cb6f72f511248a2051bfd5f3ab
zosiek23 <zosiek23@hotmail.com>
2015-03-28 15:40:37 +0000

    Swirl programming _ wk 4 --> up to function lesson 8

---
+++ SwirlAss_wk4.R
@@ -0,0 +1,13 @@
+# /install packages
+install.packages("swirl")
+library(swirl)
+swirl()
+
+
+# swirl offers a variety of interactive courses, but for our purposes,
+# you want the one called R Programming. 
+# If this is your first time using swirl, it will prompt you 
+# to install the R Programming course automatically. 
+# If you've used swirl in the past, you will need to type 
+# the following from the R prompt:
+install_from_swirl("R Programming")
